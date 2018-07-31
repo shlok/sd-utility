de3e381a92c48c59e7874ae7e9ca161372bb16af
Shlok Datye <shlok@quant.is>
2018-07-31 21:21:51 +0200

    Added INLINE pragmas.

--- src/SD/Utility/MaxLengthSequence.hs
+++ src/SD/Utility/MaxLengthSequence.hs
@@ -35,16 +35,20 @@ empty n
     | n <= 0 = Nothing
     | otherwise = Just $ MLSeq { maxLength = n
                                , elemsSeq = Seq.empty }
+{-# INLINE empty #-}
 
 head :: MLSeq a -> Maybe a
 head (MLSeq { elemsSeq = Empty }) = Nothing
 head (MLSeq { elemsSeq = head' :<| _ }) = Just head'
+{-# INLINE head #-}
 
 length :: MLSeq a -> Int
 length = Seq.length . elemsSeq
+{-# INLINE length #-}
 
 full :: MLSeq a -> Bool
 full s = maxLength s == length s
+{-# INLINE full #-}
 
 append :: a -> MLSeq a -> MLSeq a
 append a s @ (MLSeq { elemsSeq = Empty }) = s { elemsSeq = singleton a }
@@ -52,5 +56,6 @@ append a s @ (MLSeq { elemsSeq = currSeq @ (_ :<| currSeqTail) }) =
     if full s
      then s { elemsSeq = currSeqTail |> a }
      else s { elemsSeq = currSeq |> a }
+{-# INLINE append #-}
 
 --------------------------------------------------------------------------------
