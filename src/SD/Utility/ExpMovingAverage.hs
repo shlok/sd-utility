8d74eb8b7b8d0b8a9c9247af11fadb5c21e12d7c
Shlok Datye <shlok@quant.is>
2018-08-05 22:21:11 +0200

    Renamed ExpMovingAvg to EMA.

--- src/SD/Utility/ExpMovingAverage.hs
+++ src/SD/Utility/ExpMovingAverage.hs
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------
 
 module SD.Utility.ExpMovingAverage (
-      ExpMovingAvg ()
+      EMA ()
     , empty
     , append
     , lookupEMA
@@ -13,7 +13,7 @@ import Control.Monad (guard)
 
 --------------------------------------------------------------------------------
 
-data ExpMovingAvg a = ExpMovingAvg {
+data EMA a = EMA {
       count     :: !Int
     , k         :: !a
     , currCount :: !Int
@@ -21,22 +21,22 @@ data ExpMovingAvg a = ExpMovingAvg {
     }
 
 -- | Please provide a positive count. (Returns 'Nothing' otherwise.)
-empty :: (Fractional a) => Int -> Maybe (ExpMovingAvg a)
+empty :: (Fractional a) => Int -> Maybe (EMA a)
 empty n
     | n <= 0 = Nothing
-    | otherwise = Just $ ExpMovingAvg { count = n
-                                      , k = 2 / (fromIntegral n + 1)
-                                      , currCount = 0
-                                      , currEMA = 0 }
+    | otherwise = Just $ EMA { count = n
+                             , k = 2 / (fromIntegral n + 1)
+                             , currCount = 0
+                             , currEMA = 0 }
 
-append :: (Fractional a) => a -> ExpMovingAvg a -> ExpMovingAvg a
+append :: (Fractional a) => a -> EMA a -> EMA a
 append a m =
     if currCount m < count m
      then m { currCount = currCount m + 1
             , currEMA = currEMA m + a / (fromIntegral $ count m) }
      else m { currEMA = currEMA m * (1 - k m) + a * k m }
 
-lookupEMA :: ExpMovingAvg a -> Maybe a
+lookupEMA :: EMA a -> Maybe a
 lookupEMA m = do
     guard $ currCount m == count m
     Just $ currEMA m
