db8683d7272b72dacbce6166b5b3f880ec56cfe7
Shlok Datye <shlok@quant.is>
2019-06-04 23:21:06 +0200

    Updated Stack resolver and removed useless functions.

--- src/SD/Utility/Streaming.hs
+++ src/SD/Utility/Streaming.hs
@@ -5,46 +5,15 @@
 --------------------------------------------------------------------------------
 
 module SD.Utility.Streaming (
-    streamLines
-  , mapWhile
-  , takeWhile'
-  , slidingWindow'
+    slidingWindow'
   ) where
 
 --------------------------------------------------------------------------------
 
-import Control.Monad.Except (MonadError, throwError)
-import Data.ByteString (ByteString)
-import qualified Data.ByteString.Streaming.Char8 as SB (ByteString, splitWith, toStrict)
 import Data.Sequence (Seq((:<|), Empty))
 import qualified Data.Sequence as Seq (length)
-import Streaming.Prelude (Of ((:>)), Stream, copy, mapped, slidingWindow, yield)
-import qualified Streaming.Prelude as S (filter, last, map, takeWhileM)
-
---------------------------------------------------------------------------------
-
-streamLines :: (Monad m) => SB.ByteString m r -> Stream (Of ByteString) m r
-streamLines = (mapped SB.toStrict) . SB.splitWith (=='\n')
-
---------------------------------------------------------------------------------
-
--- | Maps over a stream, ending it when a mapped element fails a condition.
--- Throws a projection of the corresponding original (unmapped) element.
--- The original return value is lost.
---
--- An example of how throwing (a projection of) the /original/ element can be useful:
--- We are mapping into Maybe values and want to end on Nothing; throwing (a projection)
--- of the original element is more informative.
-mapWhile :: (MonadError e m) => (a -> b) -> (b -> Bool) -> (a -> e) -> Stream (Of a) m r -> Stream (Of b) m ()
-mapWhile mp p ae stream = S.map snd . S.takeWhileM predM $ S.map (\a -> (a, mp a)) stream
-    where predM (a, b) = if p b
-                          then return True
-                          else throwError $ ae a
-
--- | Ends a stream when an element fails a condition. Throws a projection
--- of the failed element. The original return value is lost.
-takeWhile' :: (MonadError e m) => (a -> Bool) -> (a -> e) -> Stream (Of a) m r -> Stream (Of a) m ()
-takeWhile' = mapWhile id
+import Streaming.Prelude (Of ((:>)), Stream, copy, slidingWindow, yield)
+import qualified Streaming.Prelude as S (filter, last)
 
 --------------------------------------------------------------------------------
 
--- test/SD/Utility/Streaming/Tests.hs
+++ test/SD/Utility/Streaming/Tests.hs
@@ -8,17 +8,11 @@ module SD.Utility.Streaming.Tests (tests) where
 
 --------------------------------------------------------------------------------
 
-import SD.Utility.Streaming
+import SD.Utility.Streaming (slidingWindow')
 
-import Control.Monad.Except (ExceptT, runExceptT)
-import Data.ByteString (pack, unpack)
-import Data.ByteString.Streaming (fromChunks)
-import Data.List (intercalate)
 import Data.Sequence (fromList, index, singleton)
 import qualified Data.Sequence as Seq (length)
-import Data.Word (Word8)
-import SD.Utility.Random (randomSplitList)
-import Streaming.Prelude (Of ((:>)), Stream, each, toList, toList_, yield)
+import Streaming.Prelude (each, toList_, yield)
 import Test.QuickCheck (NonEmptyList (NonEmpty), Positive (Positive))
 import Test.QuickCheck.Monadic (monadicIO, pick, run)
 import Test.Tasty (TestTree)
@@ -29,67 +23,11 @@ import Test.Tasty.QuickCheck (arbitrary, testProperty)
 
 tests :: [TestTree]
 tests =
-    [ testLines
-    , testMapWhile
-    , testTakeWhile
-    , testSlidingWindow_1
+    [ testSlidingWindow_1
     , testSlidingWindow_2
     , testSlidingWindow_3
     , testSlidingWindow_4
     , testSlidingWindow_5 ]
-
---------------------------------------------------------------------------------
-
-testLines :: TestTree
-testLines = testProperty "streamLines" $ monadicIO $ do
-    -- Start with a random list of [Word8] lists.
-    -- Each [Word8] is supposed to represent a strict bytestring line.
-    wordLists :: [[Word8]] <- pick arbitrary
-    
-    -- Filter newlines (10) out of our lines to make them “real lines.”
-    let wordLists' = map (filter (/=10)) wordLists
-
-    -- Join the lines with newlines into a one [Word8], randomly split
-    -- it into a [[Word8]], and convert the result to a [ByteString].
-    -- The idea is that we now have a random list of strict bytestrings
-    -- that are not necessarily separated at the newline boundaries. (The
-    -- newlines are buried somewhere within the bytestrings themselves.)
-    let joined = intercalate [10] wordLists'
-    randomlySplitted <- run $ randomSplitList joined
-    let byteStrings = map pack randomlySplitted
-
-    -- Turn the random bytestrings into a streaming bytestring, use
-    -- our streamLines function (the one we’re testing) to get our strict
-    -- bytestring lines back, and verify that these lines are correct.
-    lineByteStrings :> _ <- run . toList . streamLines . fromChunks $ each byteStrings
-    let wordLists'' = map unpack lineByteStrings
-    return $ if wordLists' == [[]]
-              then wordLists'' == [] -- Special case.
-              else wordLists' == wordLists''
-
---------------------------------------------------------------------------------
-
-testMapWhile :: TestTree
-testMapWhile =
-    testCase "mapWhile" $ do
-        let stream = each [1..100]
-        let stream' :: Stream (Of Int) (ExceptT String IO) () = mapWhile (*2) (<50) show stream
-        let stream'' :: Stream (Of Int) (ExceptT String IO) () = mapWhile (*2) (<500) show stream
-        eRes' <- runExceptT $ toList_ stream'
-        assertEqual "Expected failure" eRes' (Left "25")
-        eRes'' <- runExceptT $ toList_ stream''
-        assertEqual "Expected success" eRes'' (Right [2,4..200])
-
-testTakeWhile :: TestTree
-testTakeWhile =
-    testCase "takeWhile" $ do
-        let stream = each [1..100]
-        let stream' :: Stream (Of Int) (ExceptT String IO) () = takeWhile' (<50) show stream
-        let stream'' :: Stream (Of Int) (ExceptT String IO) () = takeWhile' (<500) show stream
-        eRes' <- runExceptT $ toList_ stream'
-        assertEqual "Expected failure" eRes' (Left "50")
-        eRes'' <- runExceptT $ toList_ stream''
-        assertEqual "Expected success" eRes'' (Right [1..100])
 
 --------------------------------------------------------------------------------
 
