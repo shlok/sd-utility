7f7327640a5340ab786e6363dd37a3e200a01514
Shlok Datye <shlok@quant.is>
2019-08-31 09:55:39 +0200

    Improve Data.Store.Tests to cover data types with multiple numeric types.

--- test/Data/Store/Tests.hs
+++ test/Data/Store/Tests.hs
@@ -9,11 +9,12 @@ module Data.Store.Tests (tests) where
 --------------------------------------------------------------------------------
 
 import Data.ByteString (ByteString, concat)
-import Data.Serialize.IEEE754 (putFloat64be, putFloat64le)
-import Data.Serialize.Put (runPut)
+import Data.Serialize.IEEE754 (putFloat32be, putFloat32le, putFloat64be, putFloat64le)
+import Data.Serialize.Put (putWord16host, putWord32host, putWord64host, runPut)
 import Data.Store (Store, encode)
+import Data.Word (Word16, Word32, Word64)
 import GHC.Generics (Generic)
-import Prelude hiding (concat)
+import Prelude hiding (concat, encodeFloat)
 import System.Endian (Endianness (BigEndian, LittleEndian), getSystemEndianness)
 import Test.Tasty (TestTree)
 import Test.Tasty.QuickCheck (testProperty)
@@ -22,34 +23,63 @@ import Test.Tasty.QuickCheck (testProperty)
 
 tests :: [TestTree]
 tests =
-    [ testDoublesEncoding ]
+    [ testNumbersEncoding ]
 
 --------------------------------------------------------------------------------
 
--- Data.Store is supposed to encode a data type containing only Doubles as simply
--- a concatenation of each of the Doubles encoded in machine representation.
+-- Data.Store is supposed to encode a data type containing only numbers of the standard
+-- types as simply a concatenation of each of the numbers encoded in machine representation.
 -- We test this property here.
 
-data Doubles
-   = Doubles { _d1 :: !Double
-             , _d2 :: !Double
-             , _d3 :: !Double
-             , _d4 :: !Double
-             , _d5 :: !Double
+data Numbers
+   = Numbers { _n1 :: !Double
+             , _n2 :: !Float
+             , _n3 :: !Double
+             , _n4 :: !Word32
+             , _n5 :: !Float
+             , _n6 :: !Word64
+             , _n7 :: !Word16
+             , _n8 :: !Double
              } deriving (Generic)
 
-instance Store Doubles
+instance Store Numbers
 
-testDoublesEncoding :: TestTree
-testDoublesEncoding = testProperty "Encode data type with multiple Doubles" $
-    \d1 d2 d3 d4 d5 ->
-        encode (Doubles d1 d2 d3 d4 d5) == concat (map encodeDouble [d1, d2, d3, d4, d5])
+testNumbersEncoding :: TestTree
+testNumbersEncoding = testProperty "Encode data type with multiple numbers" $
+    \n1 n2 n3 n4 n5 n6 n7 n8 ->
+        encode (Numbers n1 n2 n3 n4 n5 n6 n7 n8)
+            == concat [ encodeDouble n1
+                      , encodeFloat n2
+                      , encodeDouble n3
+                      , encodeWord32 n4
+                      , encodeFloat n5
+                      , encodeWord64 n6
+                      , encodeWord16 n7
+                      , encodeDouble n8 ]
 
--- | Encode a single 'Double' in machine representation using the cereal library.
+--------------------------------------------------------------------------------
+
+-- The following functions encode numbers using the cereal library.
+
+encodeWord16 :: Word16 -> ByteString
+encodeWord16 = runPut . putWord16host
+
+encodeWord32 :: Word32 -> ByteString
+encodeWord32 = runPut . putWord32host
+
+encodeWord64 :: Word64 -> ByteString
+encodeWord64 = runPut . putWord64host
+
 encodeDouble :: Double -> ByteString
-encodeDouble = runPut .
-        case getSystemEndianness of
-            BigEndian -> putFloat64be
-            LittleEndian -> putFloat64le
+encodeDouble = runPut . endian putFloat64be putFloat64le
+
+encodeFloat :: Float -> ByteString
+encodeFloat = runPut . endian putFloat32be putFloat32le
+
+endian :: a -> a -> a
+endian be le =
+    case getSystemEndianness of
+        BigEndian -> be
+        LittleEndian -> le
 
 --------------------------------------------------------------------------------
