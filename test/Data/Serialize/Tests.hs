2c2ad330be8b4e98492a812c0d68bfc33884820b
Shlok Datye <shlok@quant.is>
2020-10-10 17:11:47 +0200

    Add tests.

--- test/Data/Serialize/Tests.hs
+++ test/Data/Serialize/Tests.hs
@@ -4,9 +4,10 @@ module Data.Serialize.Tests (tests) where
 
 --------------------------------------------------------------------------------
 
-import Data.Serialize.Put (putWord16be, putWord32be, putWord64be, runPut)
+import Data.Serialize.IEEE754 (putFloat32be, putFloat64be)
+import Data.Serialize.Put (Putter, putInt16be, putInt32be, putInt64be, putWord16be, putWord32be, putWord64be, runPut)
 import Test.Tasty (TestTree)
-import Test.Tasty.QuickCheck (testProperty)
+import Test.Tasty.QuickCheck (Negative (..), NonNegative (..), testProperty)
 
 --------------------------------------------------------------------------------
 
@@ -14,26 +15,74 @@ tests :: [TestTree]
 tests =
     [ testBigEndianOrderWord16
     , testBigEndianOrderWord32
-    , testBigEndianOrderWord64 ]
+    , testBigEndianOrderWord64
+    , testBigEndianOrderInt16NonNeg
+    , testBigEndianOrderInt32NonNeg
+    , testBigEndianOrderInt64NonNeg
+    , testBigEndianOrderInt16Neg
+    , testBigEndianOrderInt32Neg
+    , testBigEndianOrderInt64Neg
+    , testBigEndianOrderFloatNonNeg
+    , testBigEndianOrderDoubleNonNeg ]
 
 --------------------------------------------------------------------------------
 
--- A nice property of serializing unsigned integers in big-endian format is that order is preserved.
+-- When serializing any of the following in big-endian format, the order is supposed to be preserved.
+--     * Unsigned integers.
+--     * Nonnegative signed integers.
+--     * Negative signed integers.
+--     * Nonnegative floating-point numbers.
 -- We test this property here.
 
 testBigEndianOrderWord16 :: TestTree
 testBigEndianOrderWord16 = testProperty "Big-endian order (Word16)" $
-    \w1 w2 -> w1 `compare` w2 == put w1 `compare` put w2
-        where put = runPut . putWord16be
+    orderPreserveProp id putWord16be
 
 testBigEndianOrderWord32 :: TestTree
 testBigEndianOrderWord32 = testProperty "Big-endian order (Word32)" $
-    \w1 w2 -> w1 `compare` w2 == put w1 `compare` put w2
-        where put = runPut . putWord32be
+    orderPreserveProp id putWord32be
 
 testBigEndianOrderWord64 :: TestTree
 testBigEndianOrderWord64 = testProperty "Big-endian order (Word64)" $
-    \w1 w2 -> w1 `compare` w2 == put w1 `compare` put w2
-        where put = runPut . putWord64be
+    orderPreserveProp id putWord64be
+
+testBigEndianOrderInt16NonNeg :: TestTree
+testBigEndianOrderInt16NonNeg = testProperty "Big-endian order (Int16, nonnegative)" $
+    orderPreserveProp getNonNegative putInt16be
+
+testBigEndianOrderInt32NonNeg :: TestTree
+testBigEndianOrderInt32NonNeg = testProperty "Big-endian order (Int32, nonnegative)" $
+    orderPreserveProp getNonNegative putInt32be
+
+testBigEndianOrderInt64NonNeg :: TestTree
+testBigEndianOrderInt64NonNeg = testProperty "Big-endian order (Int64, nonnegative)" $
+    orderPreserveProp getNonNegative putInt64be
+
+testBigEndianOrderInt16Neg :: TestTree
+testBigEndianOrderInt16Neg = testProperty "Big-endian order (Int16, negative)" $
+    orderPreserveProp getNegative putInt16be
+
+testBigEndianOrderInt32Neg :: TestTree
+testBigEndianOrderInt32Neg = testProperty "Big-endian order (Int32, negative)" $
+    orderPreserveProp getNegative putInt32be
+
+testBigEndianOrderInt64Neg :: TestTree
+testBigEndianOrderInt64Neg = testProperty "Big-endian order (Int64, negative)" $
+    orderPreserveProp getNegative putInt64be
+
+testBigEndianOrderFloatNonNeg :: TestTree
+testBigEndianOrderFloatNonNeg = testProperty "Big-endian order (Float, nonnegative)" $
+    orderPreserveProp getNonNegative putFloat32be
+
+testBigEndianOrderDoubleNonNeg :: TestTree
+testBigEndianOrderDoubleNonNeg = testProperty "Big-endian order (Double, nonnegative)" $
+    orderPreserveProp getNonNegative putFloat64be
+
+orderPreserveProp :: (Ord b) => (a -> b) -> Putter b -> (a -> a -> Bool)
+orderPreserveProp unwrap putter =
+    \a1 a2 ->
+        let put = runPut . putter
+            (b1, b2) = (unwrap a1, unwrap a2)
+        in b1 `compare` b2 == put b1 `compare` put b2
 
 --------------------------------------------------------------------------------
