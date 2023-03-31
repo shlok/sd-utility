14327d17da0c9bdd67c694c03cde722d202c6d14
Shlok Datye <shlok@quant.is>
2023-03-31 08:10:07 +0200

    Add flake.nix. Update cabal version. Remove Stack-related things.

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "sd-utility";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        ghcVersion = "8107";
+        packageName = "sd-utility";
+
+        config = {};
+
+        overlays = [
+          (final: prev:
+            let
+              haskellPkgs = final.haskell.packages."ghc${ghcVersion}";
+            in {
+              myHaskellPackages = haskellPkgs.override {
+                overrides = hfinal: hprev: { 
+                  ${packageName} = (hfinal.callCabal2nix "${packageName}" ./. {});
+                };
+              };
+
+              ${packageName} = final.myHaskellPackages.${packageName};
+
+              myDevShell = final.myHaskellPackages.shellFor {
+                packages = p: [ p.${packageName} ];
+
+                nativeBuildInputs = [
+                  haskellPkgs.cabal-install
+                  haskellPkgs.haskell-language-server
+                ];
+              };
+            })
+        ];
+
+        pkgs = import nixpkgs { inherit config overlays system; };
+      in {
+        packages.default = pkgs.${packageName};
+        devShells.default = pkgs.myDevShell;
+      });
+}
